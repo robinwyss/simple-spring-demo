91af80aa8914f58ba4399d58ab4d1b45d2a2d9f3
Robin Wyss <me@robinwyss.com>
2025-02-05 07:51:23 +0000

    Fixed template

--- src/main/java/com/robinwyss/rapdemo/controller/HelloController.java
+++ src/main/java/com/robinwyss/rapdemo/controller/HelloController.java
@@ -1,19 +1,22 @@
 package com.robinwyss.rapdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    
+
     @GetMapping("/hello")
-    public String hello() {
-        return "H E L L O!";
+    public String hello(Model model) {
+        model.addAttribute("message", "H E L L O!");
+        return "hello";
     }
 
     @GetMapping("/hello/**")
-    public String hello2() {
-        return "H E L L O ** !";
+    public String hello2(Model model) {
+        model.addAttribute("message", "H E L L O ** !");
+        return "hello";
     }
 
 }
